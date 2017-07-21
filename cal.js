469f110cb4310163ce575bb13b0381410c299e84
me-PC\me <chaitanya.rangam46@gmail.com>
2017-07-21 16:37:36 +0530

    commit1

---
+++ cal.js
@@ -0,0 +1,35 @@
+function btplus(){
+
+			document.calculator.display.value +="+";
+			document.calculator.display.style.textAlign="right";
+}
+function btsub(){
+
+			document.calculator.display.value +="-";
+			document.calculator.display.style.textAlign="right";
+}
+function btmu(){
+
+			document.calculator.display.value +="*";
+			document.calculator.display.style.textAlign="right";
+}
+function btclear(){
+
+			document.calculator.display.value ="";
+			document.calculator.display.style.textAlign="right";
+}
+function btdiv(){
+
+			document.calculator.display.value +="/";
+			document.calculator.display.style.textAlign="right";
+}
+function btdot(){
+
+			document.calculator.display.value +=".";
+			document.calculator.display.style.textAlign="right";
+}
+function btmod(){
+
+			document.calculator.display.value +="%";
+			document.calculator.display.style.textAlign="right";
+}
